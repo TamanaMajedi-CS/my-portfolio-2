d273a1ab55f883da11cc912704035b893f15b582
TamanaMajedi-CS <tamanamajede@gmail.com>
2025-09-28 19:55:51 +0430

    second

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,23 @@
 import { NavLink } from "react-router-dom";
 import ThemeToggle from "./ThemeToggle.jsx";
 
+const base = import.meta.env.BASE_URL; 
+
 export default function Navbar(){
   return (
     <nav className="nav">
       <div className="nav-inner">
-        <NavLink to="/" className="brand">Tamana Majedi</NavLink>
+        <NavLink to="/" className="brand" aria-label="Home">
+          <img
+            src={`${base}assets/logo.svg`}   
+            alt=""
+            className="logo"
+            width="28"
+            height="28"
+          />
+          <span className="brand-text">Tamana Majedi</span>
+        </NavLink>
+
         <div className="spacer" />
         <NavLink to="/" end>Home</NavLink>
         <NavLink to="/about">About</NavLink>
